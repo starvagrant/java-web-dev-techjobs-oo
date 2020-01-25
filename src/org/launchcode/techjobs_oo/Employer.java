2fbd16dd6e0dcb98cb9311c6ea668106e81f5c55
James Ginns <starvagrant@yahoo.com>
2020-01-25 16:24:09 -0600

    Move code from Employer to Jobfield Class.

--- src/org/launchcode/techjobs_oo/Employer.java
+++ src/org/launchcode/techjobs_oo/Employer.java
@@ -2,13 +2,11 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class Employer {
-    private int id;
+public class Employer extends JobField {
     private static int nextId = 1;
-    private String value;
 
     public Employer() {
-        id = nextId;
+        this.id = nextId;
         nextId++;
     }
 
@@ -17,12 +15,7 @@ public class Employer {
         this.value = value;
     }
 
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    // Custom equals, and hashCode methods:
 
     @Override
     public boolean equals(Object o) {  // Two objects are equal if they have the same id.
@@ -35,19 +28,5 @@ public class Employer {
     @Override
     public int hashCode() {
         return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 }
